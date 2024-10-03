09937b5bbf09e7944fa56d173023f1b6368dc64f
Sohail Faiz Rasool <sa472811@gmail.com>
2024-10-03 07:48:57 +0500

    variables and values

---
+++ 1_simplevalues/1.1_simple_values.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(1)
+	fmt.Println(1+1)
+	//char
+	fmt.Println('a')
+
+	fmt.Println("ITS A STRING")
+	fmt.Println(true, false)
+	// its a floot
+	fmt.Println(79.28)
+
+}
---
+++ 2_variables/3.1_variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "sohail"
+	var age int  = 22 
+	fmt.Println(name)
+	fmt.Println(age)
+
+	var newVar= "Sohail Faiz"
+	fmt.Println(newVar)
+
+	name1:="Ali"
+	fmt.Println(name1)
+}	
